b4521b8fa8c5ae4a73f69f2e1ae8576f22f9dd8b
Akanimoh Osutuk <akan@Akanimohs-MacBook-Pro.local>
2022-03-12 23:29:17 +0100

    added a new deployment script

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,25 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    address[] public arr;
+
     constructor() {
         console.log("Learning solidity again");
+    }
+
+    function wave() public {
+        totalWaves++;
+        console.log("Someone just waved: %s", msg.sender);
+        arr.push(msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("Total number of wave is: %d", totalWaves);
+        return totalWaves;
+    }
+
+    function getAddress() public view returns (address[] memory) {
+        return arr;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,25 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-    console.log("Contract deployed to: ", waveContract.address);
+
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTnx = await waveContract.wave();
+    await waveTnx.wait();
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTnx = await waveContract.connect(randomPerson).wave();
+    await waveTnx.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    let add = await waveContract.getAddress();
 }
 
 const runMain = async () => {
